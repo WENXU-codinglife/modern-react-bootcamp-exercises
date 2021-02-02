fe24f5e5e2a17de8da699012f7547d90c359b270
wenxuw <xu_wen@sfu.ca>
2021-02-01 19:16:43 -0800

    rolldice

---
+++ s07-dice/POINTS.md
@@ -0,0 +1,4 @@
+1. fontawesome for icons: add the url into index.html
+2. check how to embed 'icon'
+3. automation by css 
+4. css effects depending on conditions
--- s07-dice/src/Die.js
+++ s07-dice/src/Die.js
@@ -3,6 +3,7 @@ import './Die.css';
 
 class Die extends Component {
   render() {
+    // why is 'Die' put here as a class name???
     return <i className={`Die fas fa-dice-${this.props.num} ${this.props.rolling && 'rolling'}`} />;
   }
 }
